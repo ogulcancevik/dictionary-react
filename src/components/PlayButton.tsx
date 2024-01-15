5a92ba62adb6c3381a123fca5dddf7bc0f33df31
ogulcancevik <cancevdev@gmail.com>
2024-01-15 16:50:04 +0300

    Add pause functionality to PlayButton component

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,21 +1,22 @@
-import { IoPlay } from 'react-icons/io5'
+import { IoPlay, IoPause } from 'react-icons/io5'
 import { IPhonetic } from '../types/dictionary.types'
-
+import { useState } from 'react'
 const PlayButton = ({ phonetics }: { phonetics: IPhonetic[] }) => {
+  const [isPlaying, setIsPlaying] = useState(false)
   const getAudio = (phonetics: IPhonetic[]) => {
     return phonetics.find((item) => item.audio !== '')
   }
   const audio = getAudio(phonetics)
-  const playAudio =  () => {
-    if (!audio) {
-      alert('No audio found')
-    }
-    const audioElement = new Audio(audio?.audio)
-    audioElement.play()
+  const playAudio =  async () => {
+    if (!audio) return
+    const audioElement = new Audio(audio.audio)
+    setIsPlaying(true)
+    await audioElement.play()
+    audioElement.onended = () => setIsPlaying(false)
   }
   return (
     <div className="bg-[#a245ed] bg-opacity-30 rounded-full w-14 h-14 flex justify-center items-center cursor-pointer" onClick={playAudio}>
-      <IoPlay size={24} color="#a245ed" />
+      {isPlaying ? <IoPause size={24} color="#a245ed" /> : <IoPlay size={24} color="#a245ed" />}
     </div>
   )
 }
